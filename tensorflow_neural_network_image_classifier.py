258ab0c4ffc5072fda32571e1c160ce0fa1c6dd0
mamun-131 <mamun_131@hotmail.com>
2023-02-06 22:01:15 -0500

    update done

--- tensorflow_neural_network_image_classifier.py
+++ tensorflow_neural_network_image_classifier.py
@@ -80,3 +80,69 @@ logdir='logs'
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
 #----------------------------------------------------
+# Plot Performance -----------------------------------------
+fig = plt.figure()
+plt.plot(hist.history['loss'], color='teal', label='loss')
+plt.plot(hist.history['val_loss'], color='orange', label='val_loss')
+fig.suptitle('Loss', fontsize=20)
+plt.legend(loc="upper left")
+plt.show()
+
+fig = plt.figure()
+plt.plot(hist.history['accuracy'], color='teal', label='accuracy')
+plt.plot(hist.history['val_accuracy'], color='orange', label='val_accuracy')
+fig.suptitle('Accuracy', fontsize=20)
+plt.legend(loc="upper left")
+plt.show()
+#----------------------------------------------------
+# Evaluate -----------------------------------------
+from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
+pre = Precision()
+re = Recall()
+acc = BinaryAccuracy()
+
+for batch in test.as_numpy_iterator(): 
+    X, y = batch
+    yhat = model.predict(X)
+    pre.update_state(y, yhat)
+    re.update_state(y, yhat)
+    acc.update_state(y, yhat)
+
+print(pre.result(), re.result(), acc.result())
+#----------------------------------------------------
+# Test -----------------------------------------
+import cv2
+
+img = cv2.imread('test1.jpg')
+plt.imshow(img)
+plt.show()
+
+resize = tf.image.resize(img, (256,256))
+plt.imshow(resize.numpy().astype(int))
+plt.show()
+
+yhat = model.predict(np.expand_dims(resize/255, 0))
+
+yhat
+
+if yhat > 0.5: 
+    print(f'Predicted class is Sad')
+else:
+    print(f'Predicted class is Happy')
+#----------------------------------------------------
+# Save the model -----------------------------------------
+from tensorflow.keras.models import load_model
+
+model.save(os.path.join('models','imageclassifier.h5'))
+
+new_model = load_model(os.path.join('models/','imageclassifier.h5'))
+
+new_model.predict(np.expand_dims(resize/255, 0))
+
+
+#----------------------------------------------------
+
+
+
+
+
